83d8b8cc2bfa7bd87e9add17b6a3d4b9a02023f7
Yago <yagosonicmassaroni@gmail.com>
2024-12-28 20:35:34 -0300

    pattern: factory

--- src/http/controllers/authenticate.ts
+++ src/http/controllers/authenticate.ts
@@ -1,8 +1,7 @@
 import { z } from 'zod'
 import { FastifyReply, FastifyRequest } from 'fastify'
-import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
-import { AuthenticateUseCase } from '@/use-cases/authenticate'
 import { InvalidCredentialsError } from '@/use-cases/erros/invalid-credentials-error'
+import { makeAuthenticateUseCases } from '@/use-cases/factories/make-authenticate-use-case'
 
 export async function authenticate(
   request: FastifyRequest,
@@ -16,8 +15,7 @@ export async function authenticate(
   const { email, password } = authenticateBodySchemma.parse(request.body)
 
   try {
-    const usersRepository = new PrismaUsersRepository()
-    const authenticateUseCase = new AuthenticateUseCase(usersRepository)
+    const authenticateUseCase = makeAuthenticateUseCases()
 
     await authenticateUseCase.execute({ email, password })
   } catch (err) {
--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,8 +1,7 @@
 import { z } from 'zod'
 import { FastifyReply, FastifyRequest } from 'fastify'
-import { RegisterUseCase } from '@/use-cases/register'
-import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
 import { UserAlreadyExistsError } from '@/use-cases/erros/user-already-exists-error'
+import { makeRegisterUseCases } from '@/use-cases/factories/make-register-use-cases'
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
   const registerBodySchemma = z.object({
@@ -14,8 +13,7 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
   const { name, email, password } = registerBodySchemma.parse(request.body)
 
   try {
-    const usersRepository = new PrismaUsersRepository()
-    const registerUseCase = new RegisterUseCase(usersRepository)
+    const registerUseCase = makeRegisterUseCases()
 
     await registerUseCase.execute({ name, email, password })
   } catch (err) {
