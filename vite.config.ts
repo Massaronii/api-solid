54fc9a95a4bee56218f7cc6a60761f4f38bf33b7
Yago <yagosonicmassaroni@gmail.com>
2025-01-07 22:37:09 -0300

    test2

--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,6 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 export default defineConfig({
   plugins: [tsconfigPaths()],
   test: {
-    environment: 'prisma',
-    environmentOptions: {
-      adapter: 'psql',
-      envFile: '.env.example',
-      prismaEnvVarName: 'DATABASE_URL',
-      transformMode: 'ssr',
-    },
     environmentMatchGlobs: [['src/http/controllers/**', 'prisma']],
-
-    dir: 'src', // Essa linha
   },
 })
